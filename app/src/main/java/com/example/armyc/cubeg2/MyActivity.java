634d76aec621c7c44a42ee69eadfd9f28ebd5ba4
LeoJu <armychu1989@gmail.com>
2018-11-15 10:42:36 +0800

    Init version

---
+++ app/src/main/java/com/example/armyc/cubeg2/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.armyc.cubeg2;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyActivity extends AppCompatActivity {
+    private GLSurfaceView mGLView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGLView = new MySurfaceView(this);
+        setContentView(mGLView);
+    }
+}
